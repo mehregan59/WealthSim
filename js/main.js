5752282f66d82105daa66ddbedf9d55d29853564
Mehregan <90446634+mehregan59@users.noreply.github.com>
2026-07-25 00:29:10 +0200

    feat: add PlayerSetup, RetirementContext, StartingQuestions scenes to flow

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
-const config={type:Phaser.AUTO,width:1280,height:720,backgroundColor:'#0a1628',scale:{mode:Phaser.Scale.FIT,autoCenter:Phaser.Scale.CENTER_BOTH,parent:'game-container'},scene:[Boot,GameScene],audio:{disableWebAudio:false}};
-const game=new Phaser.Game(config);
+const config = {
+  type: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  backgroundColor: '#060e1c',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    parent: 'game-container'
+  },
+  scene: [Boot, PlayerSetup, RetirementContext, StartingQuestions, GameScene],
+  audio: { disableWebAudio: false }
+};
+
+const game = new Phaser.Game(config);
